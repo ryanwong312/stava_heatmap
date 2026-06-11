b0eabd6f2fe0ea296cd96b7ba0b002f767c6bb38
ryanwong312 <wongryan312@gmail.com>
2026-06-11 08:19:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -262,4 +262,4 @@ def api_delete_activity(activity_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
